282ec72b661709bd5a857bde3aac27ad81df6ff9
Dmytro-77 <108612579+Dmytro-77@users.noreply.github.com>
2023-02-05 20:08:42 +0200

    ff

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-hello = "hello world"
+hello = "hello world g"
 console.log(hello);
 console.log(galleryItems);
